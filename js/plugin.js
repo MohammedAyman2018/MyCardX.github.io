73a2127c6936f1f191ea5845908bf82887ec658e
Mohammed Ayman <kookoo_1970@yahoo.com>
2019-03-27 19:26:02 +0200

    Finish the home banner.

--- js/plugin.js
+++ js/plugin.js
@@ -6,8 +6,8 @@ $(window).on('load', function() {
 
   $(window).on('scroll', function() {
     console.log($('nav')[0].classList, window.pageYOffset, `offset ${offset}`)
-    if (window.pageYOffset > offset) $('nav')[0].classList.add("sticky");
-    else $('nav')[0].classList.remove("sticky");
+    if (window.pageYOffset > offset) $('nav').addClass('bgt');
+    else $('nav').removeClass('bgt');
   })
 
   btn.on('click', function() {
@@ -20,6 +20,34 @@ $(window).on('load', function() {
   $(li).mouseover(function() {
     $(li).removeClass('active');
     $(this).addClass('active');
+  });
+
+  $(".typed").typed({
+    strings: ["Developer.", "Designer.", "Freelancer."],
+    // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+    stringsElement: null,
+    // typing speed
+    typeSpeed: 30,
+    // time before typing starts
+    startDelay: 1200,
+    // backspacing speed
+    backSpeed: 20,
+    // time before backspacing
+    backDelay: 500,
+    // loop
+    loop: true,
+    // false = infinite
+    loopCount: 5,
+    // show cursor
+    showCursor: true,
+    // character for cursor
+    cursorChar: "|"
+  });
+
+  $('a[href="#aboutMe"]').on('click' , function(){
+    $('html, body').animate({
+      scrollTop: $('#aboutMe').offset().top
+    }, 500);
   });
 });
 
