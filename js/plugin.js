5bf0f897c13ea3aacf20843da038de93173f7e08
Mohammed Ayman <kookoo_1970@yahoo.com>
2019-03-29 20:11:01 +0200

    More Orginaized & in Portofolio & More JS Work

--- js/plugin.js
+++ js/plugin.js
@@ -4,9 +4,9 @@ $(window).on('load', function() {
     li = $('.nav ul li'),
     offset = $('nav').offset().top;
 
+  // ==================== nav ============================= \\
   $(window).on('scroll', function() {
-    console.log($('nav')[0].classList, window.pageYOffset, `offset ${offset}`)
-    if (window.pageYOffset > offset) $('nav').addClass('bgt');
+    if (window.pageYOffset > 600) $('nav').addClass('bgt');
     else $('nav').removeClass('bgt');
   })
 
@@ -16,18 +16,18 @@ $(window).on('load', function() {
     });
     $(nav).css('display', 'flex');
   });
-
+  // nav li active
   $(li).mouseover(function() {
     $(li).removeClass('active');
     $(this).addClass('active');
   });
-
+  // ==================== typed js ============================= \\
   $(".typed").typed({
     strings: ["Developer.", "Designer.", "Freelancer."],
     // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
     stringsElement: null,
     // typing speed
-    typeSpeed: 30,
+    typeSpeed: 20,
     // time before typing starts
     startDelay: 1200,
     // backspacing speed
@@ -37,20 +37,61 @@ $(window).on('load', function() {
     // loop
     loop: true,
     // false = infinite
-    loopCount: 5,
+    loopCount: 9,
     // show cursor
     showCursor: true,
     // character for cursor
     cursorChar: "|"
   });
-
-  // ==================== Debug Me ============================= \\ 
-  $('a[href="#aboutMe"]').on('click' , function(){
+  // ==================== Scroll behaviour ============================= \\
+  $('a').on('click' ,function(){
+    let ref = $(this).attr('href');
     $('html, body').animate({
-      scrollTop: $('#aboutMe').offset().top
+      scrollTop: $(ref).offset().top
     }, 500);
   });
+  
+  // ==================== Gallery ============================= \\
+    var gallery = $('#gallery'),
+    close = $('#gallery button'),
+    open = $('.open-gallery'),
+    label_src = $("[data-label]"),
+    label = $("#heading");
+    
+    label_src.click(function(){
+      let image_data = $(this).attr('data-label');
+      let targetH2 = $(`h2[data-label = ${image_data}]`).html();
+      
+      label.html(targetH2);
+    });
 
+    open.click(function(e){
+      gallery.fadeIn(1000)
+      e.preventDefault();
+    });
+
+    close.click(function (e) {
+      gallery.slideUp();
+      e.preventDefault();
+    });
+  // ==================== Owl Carousel ============================= \\
+  $('.carousel').carousel('pause');
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+  });
   
 });
 
